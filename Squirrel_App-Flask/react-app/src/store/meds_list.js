d87fca29aaf9ddf9e30691fda03874c5b22c1c38
Alex <alex.hertel87@gmail.com>
2021-07-29 12:07:28 -0500

    added new_med thunk & edtited list blueprint route

--- Squirrel_App-Flask/react-app/src/store/meds_list.js
+++ Squirrel_App-Flask/react-app/src/store/meds_list.js
@@ -28,4 +28,17 @@ const delete_med = (med) => ({
 //? -*-*-*-*-*-*-*-*-*-*- Thunks -*-*-*-*-*-*-*-*-*-*-//
 //* **********************************************************/
 
-
+export const new_med_thunk = (med) => async (dispatch) => {
+    const res = await fetch('/api/active_list/new', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(med),
+    })
+    if (res.ok) {
+        const meds = await res.json()
+        dispatch(new_med(meds))
+        return meds
+    }
+}
