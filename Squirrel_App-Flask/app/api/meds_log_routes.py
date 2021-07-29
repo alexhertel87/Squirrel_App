36383697e672426461da7328b726512bf251d056
Alex <alex.hertel87@gmail.com>
2021-07-29 11:52:32 -0500

    Created store and component files

--- Squirrel_App-Flask/app/api/meds_log_routes.py
+++ Squirrel_App-Flask/app/api/meds_log_routes.py
@@ -4,3 +4,17 @@ from app.api.auth_routes import validation_errors_to_error_messages
 from app.models import Meds_List, Meds_Log, User, db
 
 meds_log_routes = Blueprint('meds_log_route', __name__)
+
+#* *-*-*-*-*-*-* All Logged Meds Route [GET] *-*-*-*-*-*-*
+
+@meds_log_routes.route('/medications/log', methods=['GET'])
+@login_required
+def meds_log():
+    logged_meds = Meds_Log.query.filter_by(user_id=current_user.id).all()
+    return {'logged_meds': [logged.to_dict() for logged in logged_meds]}
+
+
+#* *-*-*-*-*-*-* Add Meds to Logged Route [POST] *-*-*-*-*-*-*
+
+#! Need help here
+
