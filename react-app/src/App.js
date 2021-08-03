91ed0791c86b4724fb25f6cc051db8474adec01a
Alex <alex.hertel87@gmail.com>
2021-08-03 16:19:09 -0500

    pull request

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
-import NavBar from './components/NavBar';
+// import NavBar from './components/Navbar/NavBar';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
+import NavBar from './components/Navbar/NavBar';
+
 
 function App() {
   const [loaded, setLoaded] = useState(false);
