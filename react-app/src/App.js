3e1c3fb147063e5ee29f03db361441679be82f3a
Alex <alex.hertel87@gmail.com>
2021-08-03 16:25:55 -0500

    fixed issue with import on App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import LoginForm from './components/NavBar/LoginFormModal/LoginForm';
+import LoginForm from './components/NavBar/LoginFormModal/LoginForm.js';
 import NavBar from './components/NavBar/NavBar';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
